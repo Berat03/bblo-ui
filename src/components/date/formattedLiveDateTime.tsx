906403ad03fb8c60194670e29495857b3344234d
berat03 <bbulbulwork@gmail.com>
2024-10-07 11:00:05 +0100

    feat(radialdonut): separate out rendering and procesing of donut

--- src/components/date/formattedLiveDateTime.tsx
+++ src/components/date/formattedLiveDateTime.tsx
@@ -1,28 +1,21 @@
-import { useState, useEffect } from 'react';
-
-export const FormattedLiveDateTime = () => {
-    const [date, setDate] = useState(new Date());
-
-    useEffect(() => {
-        const timer = setInterval(() => setDate(new Date()), 1000);
-        return function cleanup() {
-            clearInterval(timer);
-        };
-    }, []);
-
-    const formattedDate = date.toLocaleDateString('en-GB', {
-        weekday: 'short',  // 'Tue'
-        day: 'numeric',    // '17'
+interface FormattedLiveDateTimeProps {
+    date: Date;
+}
+export const FormattedLiveDateTime = (props: FormattedLiveDateTimeProps) => {
+    const formattedDate = props.date.toLocaleDateString('en-GB', {
+        weekday: 'short', // 'Tue'
+        day: 'numeric' // '17'
     });
 
-    const formattedTime = date.toLocaleTimeString('en-GB', {
+    const formattedTime = props.date.toLocaleTimeString('en-GB', {
         hour: '2-digit',
         minute: '2-digit',
-        second: '2-digit',  // Include seconds
         hour12: false // 24-hour format
     });
-
+    console.log('refreshed');
     return (
-            <p>{formattedDate} {formattedTime}</p>
+        <>
+            {formattedDate} {formattedTime}
+        </>
     );
 };
