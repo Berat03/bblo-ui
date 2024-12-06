6a1d9fd9f096fa0da944dc85d39467d9bde961e6
berat03 <bbulbulwork@gmail.com>
2024-12-06 13:19:32 +0000

    feat: implement vercel web analytics, tbt

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App.tsx';
 import './app/globals.css';
-
+import { Analytics } from "@vercel/analytics/react"
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
+        <Analytics/>
     </StrictMode>
 );
